9a8eb8de98a28c7bda6f46359f37c5c3618c3152
Romain Péchot <romain.pechot@gmail.com>
2025-11-27 07:58:17 +0100

    First cleanup

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
-public class Flocking : MonoBehaviour
+public class Fish : MonoBehaviour
 {
-    public Vector3 baseRotation;
-
     [Range(0, 10)]
     public float maxSpeed = 1f;
 
@@ -14,6 +13,9 @@ public class Flocking : MonoBehaviour
 
     [Range(1, 10)]
     public float neighborhoodRadius = 3f;
+
+    [Range(0.1f, 10f)]
+    public float separationRadius = 1f;
 
     [Range(0, 3)]
     public float separationAmount = 1f;
@@ -27,36 +29,26 @@ public class Flocking : MonoBehaviour
     public Vector2 acceleration;
     public Vector2 velocity;
 
-    private Vector2 Position {
-        get {
-            return gameObject.transform.position;
-        }
-        set {
-            gameObject.transform.position = value;
-        }
-    }
-
     private void Start()
     {
         float angle = Random.Range(0, 2 * Mathf.PI);
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
     }
 
     private void Update()
     {
-        var boidColliders = Physics2D.OverlapCircleAll(Position, neighborhoodRadius);
-        var boids = boidColliders.Select(o => o.GetComponent<Flocking>()).ToList();
+        var boidColliders = Physics2D.OverlapCircleAll(transform.position, neighborhoodRadius);
+        var boids = boidColliders.Select(boidCollider => boidCollider.GetComponent<Fish>()).ToList();
         boids.Remove(this);
 
-        Flock(boids);
+        ComputeAcceleration(boids);
         UpdateVelocity();
         UpdatePosition();
         UpdateRotation();
-        WrapAround();
     }
 
-    private void Flock(IEnumerable<Flocking> boids)
+    private void ComputeAcceleration(IEnumerable<Fish> boids)
     {
         var alignment = Alignment(boids);
         var separation = Separation(boids);
@@ -73,16 +65,16 @@ public class Flocking : MonoBehaviour
 
     private void UpdatePosition()
     {
-        Position += velocity * Time.deltaTime;
+        transform.Translate(velocity * Time.deltaTime, Space.World);
     }
 
     private void UpdateRotation()
     {
         var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
-    private Vector2 Alignment(IEnumerable<Flocking> boids)
+    private Vector2 Alignment(IEnumerable<Fish> boids)
     {
         var velocity = Vector2.zero;
         if (!boids.Any()) return velocity;
@@ -91,41 +83,41 @@ public class Flocking : MonoBehaviour
         {
             velocity += boid.velocity;
         }
-        velocity /= boids.Count();
 
+        velocity /= boids.Count();
         var steer = Steer(velocity.normalized * maxSpeed);
         return steer;
     }
 
-    private Vector2 Cohesion(IEnumerable<Flocking> boids)
+    private Vector2 Cohesion(IEnumerable<Fish> boids)
     {
         if (!boids.Any()) return Vector2.zero;
 
         var sumPositions = Vector2.zero;
         foreach (var boid in boids)
         {
-            sumPositions += boid.Position;
+            sumPositions += (Vector2)boid.transform.position;
         }
-        var average = sumPositions / boids.Count();
-        var direction = average - Position;
 
+        var average = sumPositions / boids.Count();
+        var direction = average - (Vector2)transform.position;
         var steer = Steer(direction.normalized * maxSpeed);
         return steer;
     }
 
-    private Vector2 Separation(IEnumerable<Flocking> boids)
+    private Vector2 Separation(IEnumerable<Fish> boids)
     {
         var direction = Vector2.zero;
-        boids = boids.Where(o => DistanceTo(o) <= neighborhoodRadius / 2);
+        boids = boids.Where(boid => Vector2.Distance(transform.position, boid.transform.position) <= separationRadius);
         if (!boids.Any()) return direction;
 
         foreach (var boid in boids)
         {
-            var difference = Position - boid.Position;
-            direction += difference.normalized / difference.magnitude;
+            Vector2 difference = transform.position - boid.transform.position;
+            direction += difference.normalized;
         }
-        direction /= boids.Count();
 
+        direction /= boids.Count();
         var steer = Steer(direction.normalized * maxSpeed);
         return steer;
     }
@@ -134,13 +126,7 @@ public class Flocking : MonoBehaviour
     {
         var steer = desired - velocity;
         steer = LimitMagnitude(steer, maxForce);
-
         return steer;
-    }
-
-    private float DistanceTo(Flocking boid)
-    {
-        return Vector3.Distance(boid.transform.position, Position);
     }
 
     private Vector2 LimitMagnitude(Vector2 baseVector, float maxMagnitude)
@@ -149,14 +135,18 @@ public class Flocking : MonoBehaviour
         {
             baseVector = baseVector.normalized * maxMagnitude;
         }
+
         return baseVector;
     }
 
-    private void WrapAround()
+    private void OnDrawGizmosSelected()
     {
-        if (Position.x < -14) Position = new Vector2(14, Position.y);
-        if (Position.y < -8) Position = new Vector2(Position.x, 8);
-        if (Position.x > 14) Position = new Vector2(-14, Position.y);
-        if (Position.y > 8) Position = new Vector2(Position.x, -8);
+        // Neighborhood radius.
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, neighborhoodRadius);
+
+        // Separation radius.
+        Gizmos.color = Color.salmon;
+        Gizmos.DrawWireSphere(transform.position, separationRadius);
     }
 }
---
+++ Assets/Scripts/FishTank.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class FishTank : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 Size = new Vector2(16f, 9f);
+
+    [SerializeField]
+    private GameObject fishPrefab = null;
+
+    [Range(0, 300)]
+    [SerializeField]
+    private int SpawningCount;
+
+    private Fish[] fishes = null;
+
+    private void Start()
+    {
+        fishes = new Fish[SpawningCount];
+        for (int i = 0; i < SpawningCount; i++)
+        {
+            GameObject fishInstance = Instantiate(fishPrefab, transform);
+            fishInstance.gameObject.name = $"Fish {System.Guid.NewGuid()}";
+            fishes[i] = fishInstance.GetComponent<Fish>();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        // Loop around out of bound fishes.
+        int fishesCount = fishes.Length;
+        for (int i = 0; i < fishesCount; i++)
+        {
+            Fish fish = fishes[i];
+            Vector3 position = fish.transform.localPosition;
+
+            // Left border?
+            if (position.x < -Size.x * 0.5f)
+            {
+                position.x += Size.x;
+            }
+            // Right border?
+            else if (position.x > Size.x * 0.5f)
+            {
+                position.x -= Size.x;
+            }
+
+            // Top border?
+            if (position.y > Size.y * 0.5f)
+            {
+                position.y -= Size.y;
+            }
+            // Bottom border?
+            if (position.y <  -Size.y * 0.5f)
+            {
+                position.y += Size.y;
+            }
+
+            fish.transform.localPosition = position;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, Size);
+    }
+}
